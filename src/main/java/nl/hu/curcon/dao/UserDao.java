9214c2321735653a83adef9e58677eca0f07fc59
graddus <luccagraddushuijgens@gmail.com>
2018-04-13 15:37:15 +0200

    Add files via upload

--- src/main/java/nl/hu/curcon/dao/UserDao.java
+++ src/main/java/nl/hu/curcon/dao/UserDao.java
@@ -1,7 +1,8 @@
 package nl.hu.curcon.dao;
 import org.springframework.stereotype.Repository;
+
 import nl.hu.curcon.domain.User;
 @Repository
-public interface UserDao {
+public interface UserDao extends GenericDao<User> {
 	User findRoleForUsernameAndPassword(String username, String password);
 }
