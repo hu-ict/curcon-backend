768bb6d495d81e6ebf5a63e3fee175459717acec
Martinj98 <martin.dejongh@student.hu.nl>
2018-07-03 14:46:20 +0200

    fix error 500

--- src/main/java/nl/hu/curcon/filter/FirebaseInit.java
+++ src/main/java/nl/hu/curcon/filter/FirebaseInit.java
@@ -1,10 +1,11 @@
 package nl.hu.curcon.filter;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.Principal;
 import java.util.List;
+
+import javax.ws.rs.core.SecurityContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,16 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
-import nl.hu.curcon.domain.Function;
 import nl.hu.curcon.dto.FunctionDto;
 import nl.hu.curcon.service.UserService;
 
 @Service
 public class FirebaseInit {
-	@Autowired 
-	UserService userService; 
-	// This is to connect to the firebase Console App/ Project where Only ADMINS should login
+	@Autowired
+	UserService userService;
+
+	// This is to connect to the firebase Console App/ Project where Only ADMINS
+	// should login
 	public void FirebaseInitialize() throws IOException {
 		// Only initializes an app if none exist yet.
 		System.out.println("FIREBASEAPPS" + FirebaseApp.getApps().size());
@@ -30,7 +32,7 @@ public class FirebaseInit {
 			String path = "curconhu-firebase-adminsdk-gbgvn-e48f9044b6.json";
 
 			InputStream serviceAccount = getClass().getResourceAsStream(path);
-			//FileInputStream serviceAccount = new getResourceAsStream(path);
+			// FileInputStream serviceAccount = new getResourceAsStream(path);
 
 			FirebaseOptions options = new FirebaseOptions.Builder()
 					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
@@ -42,22 +44,31 @@ public class FirebaseInit {
 	}
 
 	public boolean functionInUser(String function) {
-		// FIXME dit gebeurt nu in het filter
-		// FirebaseInit();
-		String user = AuthenticationFilter.requestCtx.getSecurityContext().getUserPrincipal().getName();
-		
+		String user = null;
+		while (user == null) {
+			try {
+				SecurityContext ctx= AuthenticationFilter.requestCtx.getSecurityContext();
+				Principal principal=ctx.getUserPrincipal();
+				user=principal.getName();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		// String user =
+		// AuthenticationFilter.requestCtx.getSecurityContext().getUserPrincipal().getName();
+
 		System.out.println("FirebaseCheck for function:" + function + "&user" + user);
-		
-		List<FunctionDto> functions=userService.findFunctionsByUsername(user);
-		if (functions == null){
+
+		List<FunctionDto> functions = userService.findFunctionsByUsername(user);
+		if (functions == null) {
 			System.out.println("gebruiker heeft geen functions en mag helemaal niks!");
 			return false;
 		}
-		for(FunctionDto fDto : functions){
-			if (fDto.getName().equals(function)){
+		for (FunctionDto fDto : functions) {
+			if (fDto.getName().equals(function)) {
 				return true;
 			}
-			
+
 		}
 		return false;
 	}
